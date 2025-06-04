8b78d3f4be97ebacb631e2ddde50490bbeb8176a
liudi4046 <liudi404637575@gmail.com>
2025-06-04 14:17:55 +0800

    update test resequence

--- .github/scripts/test_resequence.sh
+++ .github/scripts/test_resequence.sh
@@ -19,12 +19,6 @@ get_latest_l2_batch() {
     echo "$latest_batch_dec"
 }
 
-get_latest_l1_verified_batch() {
-    current_batch=$(cast logs --rpc-url "$(kurtosis port print cdk-v1 el-1-geth-lighthouse rpc)" --address 0x1Fe038B54aeBf558638CA51C91bC8cCa06609e91 --from-block 0 --json | jq -r '.[] | select(.topics[0] == "0x9c72852172521097ba7e1482e6b44b351323df0155f97f4ea18fcec28e1f5966" or .topics[0] == "0xd1ec3a1216f08b6eff72e169ceb548b782db18a6614852618d86bb19f3f9b0d3") | .topics[1]' | tail -n 1 | sed 's/^0x//')
-    current_batch_dec=$((16#$current_batch))
-    echo "$current_batch_dec"
-}
-
 
 wait_for_l1_batch() {
     local timeout=$1
@@ -50,11 +44,8 @@ wait_for_l1_batch() {
         if [ "$batch_type" = "virtual" ]; then
             current_batch=$(cast logs --rpc-url "$(kurtosis port print cdk-v1 el-1-geth-lighthouse rpc)" --address 0x1Fe038B54aeBf558638CA51C91bC8cCa06609e91 --from-block 0 --json | jq -r '.[] | select(.topics[0] == "0x3e54d0825ed78523037d00a81759237eb436ce774bd546993ee67a1b67b6e766") | .topics[1]' | tail -n 1 | sed 's/^0x//')
             current_batch=$((16#$current_batch))
-        elif [ "$batch_type" = "verified" ]; then
-            # TODO: zkevm_verifiedBatchNumber API was removed. This test needs to be re-designed.
-            current_batch=$(cast rpc zkevm_verifiedBatchNumber --rpc-url "$(kurtosis port print cdk-v1 cdk-erigon-rpc-001 rpc)" | sed 's/^"//;s/"$//')
         else
-            echo "Invalid batch type. Use 'virtual' or 'verified'."
+            echo "Invalid batch type. Use 'virtual'."
             return 1
         fi
 
@@ -138,8 +129,9 @@ echo "Running loadtest using polycli"
 /usr/local/bin/polycli loadtest --rpc-url "$(kurtosis port print cdk-v1 cdk-erigon-rpc-001 rpc)" --private-key "0x12d7de8621a77640c9241b2595ba78ce443d05e94090365ab3bb5e19df82c625" --verbosity 600 --requests 2000 --rate-limit 500  --mode uniswapv3 --legacy
 
 echo "Waiting for batch virtualization"
-if ! wait_for_l1_batch 600 "virtual"; then
-    echo "Failed to wait for batch virtualization"
+L2_VIRTUALIZED_BATCH=$(wait_for_l1_batch 600 "virtual")
+if [[ $? -ne 0 || -z "$L2_VIRTUALIZED_BATCH" ]]; then
+    echo "Failed to wait for batch virtualization or get the virtualized batch number"
     exit 1
 fi
 
@@ -149,16 +141,9 @@ kurtosis service stop cdk-v1 cdk-node-001
 stop_cdk_erigon_sequencer
 
 
-# Good batch before counter overflow
-latest_verified_batch=$(get_latest_l1_verified_batch)
-
-# Rollback to the last good batch before the counter overflow on L1
-echo "Rolling back to batch $latest_verified_batch"
-cast send "0x2F50ef6b8e8Ee4E579B17619A92dE3E2ffbD8AD2" "rollbackBatches(address,uint64)" "0x1Fe038B54aeBf558638CA51C91bC8cCa06609e91" "$latest_verified_batch" --private-key "0x12d7de8621a77640c9241b2595ba78ce443d05e94090365ab3bb5e19df82c625" --rpc-url "$(kurtosis port print cdk-v1 el-1-geth-lighthouse rpc)"
-
 # For X Layer, ac and split db
-echo "Using integration tool to unwind to batch $latest_verified_batch"
-kurtosis service exec cdk-v1 cdk-erigon-sequencer-001 "integration state_stages_zkevm --config=/etc/cdk-erigon/config.yaml --unwind-batch-no=$latest_verified_batch --chain dynamic-kurtosis --datadir /home/erigon/data/dynamic-kurtosis-sequencer $INTEGRATION_TOOL_EXTRA_FLAGS"
+echo "Using integration tool to unwind to batch $L2_VIRTUALIZED_BATCH"
+kurtosis service exec cdk-v1 cdk-erigon-sequencer-001 "integration state_stages_zkevm --config=/etc/cdk-erigon/config.yaml --unwind-batch-no=$L2_VIRTUALIZED_BATCH --chain dynamic-kurtosis --datadir /home/erigon/data/dynamic-kurtosis-sequencer $INTEGRATION_TOOL_EXTRA_FLAGS"
 
 echo "Starting cdk-erigon with resequencing and counter enabled"
 kurtosis service exec cdk-v1 cdk-erigon-sequencer-001 "timeout 300s cdk-erigon --pprof=true --pprof.addr 0.0.0.0 --config /etc/cdk-erigon/config.yaml --datadir /home/erigon/data/dynamic-kurtosis-sequencer  --zkevm.sequencer-resequence-strict=false --zkevm.sequencer-resequence=true --zkevm.sequencer-resequence-reuse-l1-info-index=true"
@@ -205,12 +190,6 @@ if [ "$sequencer_hash" = "$node_hash" ]; then
     echo "The block hashes match for block number $comparison_block."
 else
     echo "The block hashes do not match for block number $comparison_block."
-    exit 1
-fi
-
-echo "Waiting for batch verification"
-if ! wait_for_l1_batch 1200 "verified"; then
-    echo "Failed to wait for batch verification"
     exit 1
 fi
 
